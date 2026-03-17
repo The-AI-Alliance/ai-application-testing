93fa7a46117d88688ca3cddb094e3f2d318c135e
Dean Wampler <dean.wampler@ibm.com>
2026-03-17 13:35:13 -0400

    New collections utilities.
    Signed-off-by: Dean Wampler <dean.wampler@ibm.com>

---
+++ src/common/collections.py
@@ -0,0 +1,62 @@
+
+def dict_permutations(dictionary: {str,any}, max_size: int = -1) -> [{str,any}]:
+    """
+    From an input dictionary, where the values are collections, return an array of dictionaries
+    with all permutations of the same keys and individual values from the value collections. 
+    For example, for this input dictionary:
+    ```
+    d = {
+        'k1': ['v11', 'v12'],
+        'k2': ['v21', 'v22'],
+    }
+    ```
+
+    The following array is returned:
+    ```
+    [{'k2': 'v22', 'k1': 'v12'},
+     {'k2': 'v21', 'k1': 'v12'},
+     {'k2': 'v22', 'k1': 'v11'},
+     {'k2': 'v21', 'k1': 'v11'}]
+    ```
+
+    If `max_size` >= 0, the value arrays are effectively truncated to that size, e.g., for 
+    `max_size == 0`, `[]` will be returned, for `max_size == 1`, `[{'k1': 'v11', 'k2': 'v21'}]`
+    will be returned.
+
+    If any key has an empty values collection, no entries for that key will appear in the output.
+    For example, with `d` above, `dict_permutations(d) == dict_permutations(d | {'empty':[]})`.
+    """
+    def perm(array, d):
+        if len(d) == 0:
+            return array
+        else:
+            key, values = d.popitem()
+            if not values:
+                return perm(array, d)
+            else:
+                array2 = []
+                lenv = len(values)
+                n = lenv if max_size < 0 or lenv <= max_size else max_size
+                for value in list(values)[0:n]:
+                    kv = {key: value}
+                    if not array:
+                        array2.append(kv)
+                    else:
+                        for d2 in array:
+                            array2.append(d2 | kv)
+            return perm(array2, d)
+
+    dcopy = dictionary.copy()
+    return list(reversed(perm([], dcopy))) # return in input order!
+
+
+def mult(collection: [int], skip_zeros: bool = False) -> int:
+    """Multiple the integers in the collection, optionally _skipping zeros!_"""
+    values = collection if not skip_zeros else [n for n in collection if n]
+    if values:
+        result = 1
+        for v in values:
+            result *= v
+        return result
+    else:
+        return 0
---
+++ src/tests/common/test_collections.py
@@ -0,0 +1,119 @@
+# Unit tests for the "collections" module using Hypothesis for property-based testing.
+# https://hypothesis.readthedocs.io/en/latest/
+
+from hypothesis import given, strategies as st
+import unittest
+from pathlib import Path
+import json, os, re, sys, shutil
+
+from common.collections import dict_permutations, mult
+
+class TestCollections(unittest.TestCase):
+    """
+    Test the collections utilities.
+    """
+
+    def _check(self, dictionary: {str,any}, n:int = -1):
+        actual = dict_permutations(dictionary, max_size=n)
+        # Check the size of the returned array.
+        def _length(col):
+            lc = len(col)
+            return lc if n < 0 or lc <= n else n
+        lens = [_length(col) for col in dictionary.values()]
+        expected_len = mult(lens, skip_zeros=True)
+        # check a few special cases explicitly:
+        if len(dictionary) == 0 or n==0:
+            self.assertEqual(0, expected_len, f"n={n}, lens={lens}") 
+        elif n == 1:
+            self.assertEqual(1, expected_len, f"n={n}, lens={lens}") 
+
+        self.assertEqual(expected_len, len(actual), f"actual: {actual}, d: {dictionary}, n: {n}")
+        # Check other expected sizes:
+        expected_dict_len = sum([1 for col in dictionary.values() if len(col) > 0])
+        for i in range(expected_len):
+            for j in range(i+1, expected_len):
+                leni = len(actual[i])
+                lenj = len(actual[j])
+                self.assertEqual(expected_dict_len, leni, f"i={i}: {expected_dict_len} != {leni}, {actual[i]}")
+                self.assertEqual(expected_dict_len, lenj, f"j={j}: {expected_dict_len} != {lenj}, {actual[j]}")
+                self.assertNotEqual(actual[i], actual[j], f"{i}, {j}: {actual[i]}, d = {dictionary}")
+
+        # Adding one or more keys with empty values doesn't add to the permutations:
+        actual2 = dict_permutations(dictionary | {'empty-1': [], 'empty-2': []}, max_size=n)
+        self.assertEqual(expected_len, len(actual2))
+
+    @given(
+        st.dictionaries(st.text(min_size=1), st.sets(st.text(), max_size=4), max_size=4))
+    def test_dict_permutations(self, dictionary: {str,any}):
+        """
+        Check that dict_permutations returns a list of dicts with all permutations of single key-values
+        We use a set for the values because we want to assume uniqueness, which is how
+        this feature will be used.
+        """
+        self._check(dictionary)
+
+    @given(
+        st.dictionaries(st.text(min_size=1), st.sets(st.text(), max_size=4), max_size=4),
+        st.integers(min_value=0, max_value=4))
+    def test_dict_permutations_limit_n(self, dictionary: {str,any}, n: int):
+        """
+        Check that dict_permutations returns a list of dicts with all permutations of single key-values,
+        but truncating the input collections to size n.
+        We use a set for the values because we want to assume uniqueness, which is how
+        this feature will be used.
+        """
+        self._check(dictionary, n=n)
+
+    def test_dict_permutations_limit_n_special_cases(self):
+        """
+        Check that dict_permutations returns a list of dicts with all permutations of 
+        the single key-values, but truncating the input collections to size n.
+        We use a set for the values because we want to assume uniqueness, which is how
+        this feature will be used.
+        Manually check a few cases.
+        """
+        dictionary = {'k1': ['v11', 'v12'], 'k2': ['v21', 'v22']}
+        actualm1 = dict_permutations(dictionary, max_size=-1)
+        # Note the ordering in expectedm1; this is how the dictionaries happen to be
+        # constructed. There is no requirement for this ordering, so this assumption
+        # could be break! If so, just verify the two lists are equal in length and and
+        # they contain the same dictionary elements.
+        expectedm1 = [
+            {'k1': 'v12', 'k2': 'v22'},
+            {'k1': 'v12', 'k2': 'v21'},
+            {'k1': 'v11', 'k2': 'v22'},
+            {'k1': 'v11', 'k2': 'v21'},
+        ]
+        self.assertEqual(expectedm1, actualm1)
+
+        actual0 = dict_permutations(dictionary, max_size=0)
+        expected0 = []
+        self.assertEqual(expected0, actual0)
+
+        actual1 = dict_permutations(dictionary, max_size=1)
+        expected1 = [{'k1': 'v11', 'k2': 'v21'}]
+        self.assertEqual(expected1, actual1)
+
+    @given(st.lists(st.integers(), max_size=5))
+    def test_mult_with_not_skipping_zeros(self, ints: [int]):
+        expected = 0
+        if ints:
+            expected = 1
+            for n in ints:
+                expected *= n
+
+        self.assertEqual(expected, mult(ints))
+
+    @given(st.lists(st.integers(), max_size=5))
+    def test_mult_with_skipping_zeros(self, ints: [int]):
+        ints2 = [n for n in ints if n]
+        expected = 0
+        if ints2:
+            expected = 1
+            for n in ints2:
+                expected *= n
+
+        self.assertEqual(expected, mult(ints, skip_zeros=True))
+
+if __name__ == "__main__":
+    unittest.main()
